f2db38e6e4f88e3b722290cc8efb2e659bcda1f9
Aspheria <lorran.parrilla@gmail.com>
2023-02-18 20:53:02 -0300

    lint review

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import FruitComponent from './pages/fruitComponent';
-import Home from './pages/Home';
-
 
 function App() {
   return (
--- src/pages/fruitComponent.js
+++ src/pages/fruitComponent.js
@@ -1,13 +1,22 @@
-import {Container, Title, Image, Card, Text, TextContainer} from '../styled/style';
+import {
+  Container,
+  Title,
+  Image,
+  Card,
+  Text,
+  TextContainer,
+} from "../styled/style";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function FruitComponent() {
   const [fruits, setFruits] = useState([]);
 
   useEffect(() => {
     async function fetchFruits() {
-      const response = await fetch('http://pocs.digitalpages.com.br/mock/api/fruits-api/fruits.json');
+      const response = await fetch(
+        "http://pocs.digitalpages.com.br/mock/api/fruits-api/fruits.json"
+      );
       const data = await response.json();
       setFruits(data);
     }
@@ -19,22 +28,18 @@ function FruitComponent() {
     <Container>
       {fruits.map((fruit) => (
         <Card key={fruit.id}>
-        <Image src={fruit.photo} alt={`Photo of ${fruit.name}`} />
-        <Title>{fruit.name}</Title>
-        <TextContainer>
-        <Text>Calorias: {fruit.calories}</Text>
-          <Text>Carboidratos: {fruit.carbohydrates}</Text>
-          <Text>Fibras: {fruit.fiber}</Text>
-          <Text>Proteínas: {fruit.blubber}</Text>
-          <Text>Porção: {fruit.portion}</Text>
-        </TextContainer>
-         
+          <Image src={fruit.photo} alt={`Photo of ${fruit.name}`} />
+          <Title>{fruit.name}</Title>
+          <TextContainer>
+            <Text>Calorias: {fruit.calories}</Text>
+            <Text>Carboidratos: {fruit.carbohydrates}</Text>
+            <Text>Fibras: {fruit.fiber}</Text>
+            <Text>Proteínas: {fruit.blubber}</Text>
+            <Text>Porção: {fruit.portion}</Text>
+          </TextContainer>
         </Card>
       ))}
-
-
     </Container>
-
   );
 }
 
